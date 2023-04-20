4afa6c9756942d0b4ff39a4a3e28246e7c280743
Evi Vermeêren <90692264+EviVermeeren@users.noreply.github.com>
2023-04-20 10:28:08 +0200

    feat: database is now online on server

--- classes/Db.php
+++ classes/Db.php
@@ -1,29 +1,30 @@
-<?php 
+<?php
 
-class Db {
-        
-        private static $db; // The database connection
-    
-        public static function getInstance(){ // Get an instance of the Database
-    
-            if(self::$db !== null){ // Connection already established, reuse it
-                
-                return self::$db;
-                
-            } else { // Create a new connection
-                
-                $dsn = 'mysql:host=localhost;dbname=promptswap'; // Data Source Name
-                $username = 'evi';
-                $password = '12345';
-    
-                $options = [
-                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Throw exceptions
-                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // Fetch associative arrays
-                    PDO::ATTR_EMULATE_PREPARES => false, // Use real prepared statements
-                ];
-    
-                self::$db = new PDO($dsn, $username, $password, $options); // Create a new PDO connection
-                return self::$db; // Return the connection
-            }
+class Db
+{
+
+    private static $db; // The database connection
+
+    public static function getInstance()
+    { // Get an instance of the Database
+
+        if (self::$db !== null) { // Connection already established, reuse it
+
+            return self::$db;
+        } else { // Create a new connection
+
+            $dsn = 'mysql:host=ID391480_promptswap.db.webhosting.be;dbname=ID391480_promptswap'; // Data Source Name
+            $username = 'ID391480_promptswap';
+            $password = 'ykq78pbcXQ336r9yUn73';
+
+            $options = [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Throw exceptions
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // Fetch associative arrays
+                PDO::ATTR_EMULATE_PREPARES => false, // Use real prepared statements
+            ];
+
+            self::$db = new PDO($dsn, $username, $password, $options); // Create a new PDO connection
+            return self::$db; // Return the connection
         }
     }
+}
--- php/account.php
+++ php/account.php
@@ -3,8 +3,8 @@ include_once("../inc/bootstrap.php"); //this file contains the database connecti
 include_once("../inc/functions.inc.php"); //this file contains the functions that are used in this file
 
 if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) { //if the user is not logged in, redirect to the login page
-    header('Location: ../php/login.php');
-    exit;
+  header('Location: ../php/login.php');
+  exit;
 }
 
 $email = $_SESSION["email"]; //get the email from the session
@@ -20,61 +20,65 @@ $username = $user->getUsername(); //get the username from the database
 //the id is retrieved from the database
 //javascript:void(0) is added to the url so that the page doesn't reload when the button is clicked
 //the javascript function copyToClipboard() is called when the button is clicked
-$user_id = $user->getId();
+var_dump($user);
+$user_id = $_SESSION['id']; //get the id from the session
 $share_url = "http://localhost/AIAI-prompt-platform-main/php/account.php?id=$user_id";
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Account</title>
-    <link rel="stylesheet" href="https://use.typekit.net/kqy0ynu.css" />
-    <link rel="stylesheet" href="../css/style.css" />
-  </head>
-  <body>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Account</title>
+  <link rel="stylesheet" href="https://use.typekit.net/kqy0ynu.css" />
+  <link rel="stylesheet" href="../css/style.css" />
+</head>
+
+<body>
 
   <?php include_once("../inc/nav.inc.php"); ?> <!-- Include navigation -->
 
-    <div class="profile">
+  <div class="profile">
 
-        <div class="profileimg">
-            <img class="banner" src="../media/<?php echo $profile_banner ?>" alt="">
-            <img class="pfp" src="../media/<?php echo $profile_picture ?>" alt="">
-        </div>
-
-        <div class="profilename">
-            <h2 class="nameuser"><?php echo $username ?></h2> <!-- Here we display the username of the user -->
-            <div class="likeandfollow">
-            <a class="btnfollow" href="#">Follow</a> <!-- This button will be used to follow the user -->
-            <a class="btnfollow" href="#">Flag</a> <!-- This button will be used to flag the user -->
-            <a class="btnfollow" href="../php/editAccount.php">Edit Account</a> <!-- This button will be used to edit the account -->
-            <a class="btnfollow" id="share-btn" href="javascript:void(0)" onclick="copyToClipboard('<?php echo $share_url?>')">Share</a> <!-- This button will be used to share the account -->
-            
-        </div>
-        </div>  
-
-        <div class="profilebio">
-            <p class="biotext"><?php echo $bio ?></p> <!-- Here we display the bio of the user -->
-        </div>
+    <div class="profileimg">
+      <img class="banner" src="../media/<?php echo $profile_banner ?>" alt="">
+      <img class="pfp" src="../media/<?php echo $profile_picture ?>" alt="">
     </div>
 
-      <div class="allprompts">
-        <div>
-            <h1>All prompts by <span><?php echo $username ?></span></h1> <!-- Here we display the username of the user -->
-        </div>
+    <div class="profilename">
+      <h2 class="nameuser"><?php echo $username ?></h2> <!-- Here we display the username of the user -->
+      <div class="likeandfollow">
+        <a class="btnfollow" href="#">Follow</a> <!-- This button will be used to follow the user -->
+        <a class="btnfollow" href="#">Flag</a> <!-- This button will be used to flag the user -->
+        <a class="btnfollow" href="../php/editAccount.php">Edit Account</a> <!-- This button will be used to edit the account -->
+        <a class="btnfollow" id="share-btn" href="javascript:void(0)" onclick="copyToClipboard('<?php echo $share_url ?>')">Share</a> <!-- This button will be used to share the account -->
 
-        <div class="promptflex"> 
-          
-        <h3 style="margin-top: 50px">You don't have any prompts yet!</h3>
-        <!-- don't change the html here, you update this list with a loop with data from the database, for an example check marketplace.php -->    
+      </div>
+    </div>
 
-        </div>
+    <div class="profilebio">
+      <p class="biotext"><?php echo $bio ?></p> <!-- Here we display the bio of the user -->
+    </div>
+  </div>
 
-        <?php include_once("../inc/foot.inc.php"); ?> <!-- Include footer -->
-        <script src="../css/script.js"></script> <!-- Include script -->
-  </body>
+  <div class="allprompts">
+    <div>
+      <h1>All prompts by <span><?php echo $username ?></span></h1> <!-- Here we display the username of the user -->
+    </div>
+
+    <div class="promptflex">
+
+      <h3 style="margin-top: 50px">You don't have any prompts yet!</h3>
+      <!-- don't change the html here, you update this list with a loop with data from the database, for an example check marketplace.php -->
+
+    </div>
+
+    <?php include_once("../inc/foot.inc.php"); ?> <!-- Include footer -->
+    <script src="../css/script.js"></script> <!-- Include script -->
+</body>
+
 </html>
