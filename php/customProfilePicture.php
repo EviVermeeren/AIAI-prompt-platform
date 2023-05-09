e0918c0b375682b045d72a3b2ae12f8b5d36b69a
Evi Vermeêren <evivermeeren@hotmail.com>
2023-05-09 12:26:02 +0200

    Refactor: bug fix image does not upload

--- php/customProfilePicture.php
+++ php/customProfilePicture.php
@@ -10,7 +10,7 @@ $userDetails = $user->getUserDetails();
 $profilePicture = $userDetails['profile_picture'];
 
 if (!empty($_POST)) {
-    $target_dir = "uploads/";
+    $target_dir = '/home/site/wwwroot/uploads/'; // Replace this with the absolute path to your desired directory
     $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
     $uploadOk = 1;
     $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
